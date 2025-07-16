fbbd01f873d35820a47216f5dfbcd55d54d625e0
thejyotiranjan <thats.jyotiranjan@gmail.com>
2025-07-16 19:45:16 +0530

    add about

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -1,9 +1,10 @@
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import Image from 'next/image'
 import React from 'react'
 
 const Donate = () => {
     return (
-        <main className='flex flex-col items-center py-4'>
+        <main className='flex flex-col items-center py-4 space-y-12'>
             <h1>Donate to Iskcon Rourkela</h1>
             <div className='space-y-3 w-[80vw]'>
                 <p>Dear Devotees,</p>
@@ -11,7 +12,106 @@ const Donate = () => {
                 <p>Every contribution, big or small, helps us preserve our traditions and serve the community better.</p>
                 <p>May your kindness bring you divine blessings and inner peace.</p>
             </div>
-            <Image src={'/donate-qr.jpg'} alt={'Iskcon Rourkela Donation QR Code'} width={700} height={1000}/>
+            <div className='mx-auto'>
+                <p>
+                    Indian residents are eligible for giving a tax exempt donation
+                </p>
+                <p>
+                    under section 80G of the INCOME TAX ACT.
+                </p>
+                <p>
+                    PAN NO.: AAAT10017P
+                </p>
+                <p>
+                    For NEFT, RTGS, Bank Transfer
+                </p>
+                <p>
+                    After making the deposit / E-Payment/ Bank transfer,
+                </p>
+                <p>
+                    kindly send your transaction details by sridham.govinda@gmail.com
+                </p>
+                <p>
+                    Mob: 8658490726
+                </p>
+            </div>
+            <div className='flex gap-20'>
+                <div className=''>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>
+                                    Anna dan seva
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Devotee</TableHead>
+                                <TableHead className="text-right">Amount</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">250 &times; 21</TableCell>
+                                <TableCell>5250</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">500 &times; 21</TableCell>
+                                <TableCell>10,500</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">750 &times; 21</TableCell>
+                                <TableCell>15,750</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">1000 &times; 21</TableCell>
+                                <TableCell>21,000</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>
+                                    Bank Detail
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableHeader>
+                            <TableRow>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">Bank name</TableCell>
+                                <TableCell>UNION BANK</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">A/C name</TableCell>
+                                <TableCell>ISKCON</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">A/C no.</TableCell>
+                                <TableCell>134910100017779</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">IFSC Code</TableCell>
+                                <TableCell>UBIN0813494</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">Branch name</TableCell>
+                                <TableCell>Civil Township, Rourkela</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className='p-2 border border-black'>
+                    <Image src={'/donate-qr.jpg'} alt={'Iskcon Rourkela Donation QR Code'} width={300} height={300} />
+                </div>
+            </div>
         </main>
     )
 }
--- src/components/Homepage/Navbar.tsx
+++ src/components/Homepage/Navbar.tsx
@@ -12,10 +12,10 @@ const Navbar = () => {
   const router = useRouter();
 
   return (
-    <header className='sticky top-0 left-0 px-24 py-7 shadow-2xs bg-blue-50 z-50'>
-      <div className='flex justify-between items-center'>
+    <header className='sticky top-0 left-0 md:px-24 px-7 py-7 shadow-2xs bg-blue-50 z-50'>
+      <div className='grid grid-cols-3 justify-between items-center'>
         {/* Social links */}
-        <div className='flex gap-3'>
+        <div className='md:flex hidden gap-3'>
           {
             Object.keys(SOCIALS).map((social) => (
               <SocialLink key={SOCIALS[social].link as string} link={SOCIALS[social].link as string} icon={SOCIALS[social].icon} />
@@ -23,11 +23,16 @@ const Navbar = () => {
           }
         </div>
         {/* Iskcon rourkela icon */}
-        <Image src='/iskcon-rkl-logo.png' alt='Iskcon Rourkela Logo' width={100} height={100} className=''/>
+        <Image src='/iskcon-rkl-logo.png' alt='Iskcon Rourkela Logo' width={100} height={100} className='md:mx-auto mr-auto' />
         {/* Donation */}
-        <Button variant={'destructive'} onClick={() => router.push('/donate')}>
-          Donate
-        </Button>
+        <div className='flex basis-2 gap-6 items-center ml-auto'>
+          <Link href={'https://docs.google.com/forms/d/e/1FAIpQLSeAD2mP_ruYbwKVUmQM9KLScRJZIJtqLeXi7vkpCfjXPtkPUg/viewform?pli=1&pli=1'} className='underline text-shadow-sky-300 md:text-xl text-md animate-glow'>
+            Janmashtami Registration 2025
+          </Link>
+          <Button variant={'destructive'} onClick={() => router.push('/donate')}>
+            Donate
+          </Button>
+        </div>
       </div>
       {/* Navbar links */}
       <div className='flex justify-between pt-7 pb-2'>
