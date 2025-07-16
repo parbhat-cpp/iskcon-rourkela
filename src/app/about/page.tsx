4a3b1a73cfc33c5a912d1eae6894f539772b7765
thejyotiranjan <thats.jyotiranjan@gmail.com>
2025-07-16 19:49:15 +0530

    fix error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,8 @@ function About() {
             <h1>About us</h1>
             <p>
                 At ISKCON Rourkela, devotion is a way of life.
-                Immerse yourself in a sacred space where chanting, wisdom, and community come together to nurture the soul. Our temple is more than a place of worship; it's a hub for cultural celebration, spiritual learning, and heartfelt connection.
-                Whether you’re attending a vibrant festival, joining a peaceful evening kirtan, or simply seeking clarity in your spiritual path, ISKCON Rourkela offers a welcoming environment rooted in the teachings of Lord Krishna. From service opportunities to educational programs, every moment here is a step closer to inner peace and divine love.
+                Immerse yourself in a sacred space where chanting, wisdom, and community come together to nurture the soul. Our temple is more than a place of worship it&apos;s a hub for cultural celebration, spiritual learning, and heartfelt connection.
+                Whether you&apos;re attending a vibrant festival, joining a peaceful evening kirtan, or simply seeking clarity in your spiritual path, ISKCON Rourkela offers a welcoming environment rooted in the teachings of Lord Krishna. From service opportunities to educational programs, every moment here is a step closer to inner peace and divine love.
             </p>
         </section>
     </main>
